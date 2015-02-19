c832fbcca5ae8db77b50eb963b7be33fef0b18e5
Bonnie Chinh <bchinh@ucsd.edu>
2015-02-18 21:15:08 -0800

    names buttons with json

--- routes/names.js
+++ routes/names.js
@@ -3,3 +3,14 @@ var nav = require('../navigation.json');
 exports.view = function(req,res) {
 	res.render('names',nav);	
 };
+
+var namesList = require('../names.json');
+
+exports.view = function(req, res) {
+	res.render('names', namesList);
+};
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+};
