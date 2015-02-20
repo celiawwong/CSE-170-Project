ac09197f5de8abf3d381fd618477d635c4b99f52
Celia Wong <celiawwong@gmail.com>
2015-02-19 22:36:31 -0800

    fixed analytics page with multiple json files

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,5 +1,13 @@
 var nav = require('../navigation.json');
+var rankings = require('../rankings.json');
 
 exports.view = function(req,res) {
-	res.render('analytics',nav);	
+	var combined_json = {};
+	combined_json["navigation"] = nav;
+	combined_json['rankings'] = rankings;
+
+	console.log(combined_json);
+
+	res.render('analytics', combined_json);
+//	res.render('analytics',rankings);	
 };
